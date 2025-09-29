780cdd37e7489502430168ad541b23990fdfc263
cashworld <cashworldnet@gmail.com>
2025-09-29 13:40:20 +0100

    Add files via upload

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+
+// eslint.config.mjs
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import next from "@next/eslint-plugin-next";
+import importPlugin from "eslint-plugin-import";
+import reactNative from "eslint-plugin-react-native";
+import prettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "@next/next": next,
+      import: importPlugin,
+      "react-native": reactNative,
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+  prettier,
+];
