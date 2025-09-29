e3b17a21c85ae82c3e6b07f55f750ea20cc50b2d
cashworld <cashworldnet@gmail.com>
2025-09-29 06:26:35 -0700

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+];
